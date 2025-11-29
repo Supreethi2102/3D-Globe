f604fc91e239ffca9657936a308df406be412d8a
Rajarshi Prattipati <rishi.prattipati@raptorx.ai>
2025-11-29 12:36:56 +0530

    Fix Vercel 404 by correcting Vite outDir

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,27 @@
-import {resolve, dirname} from 'node:path';
-import {defineConfig} from 'vite';
-import {glob} from 'glob';
+import { resolve, dirname } from 'node:path';
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
 
 const exampleFiles = glob.sync(resolve(__dirname, 'examples/**/index.html'));
 const index = resolve(__dirname, 'index.html');
+
 const input = exampleFiles.reduce(
   (all, filepath) => {
     const name = dirname(filepath).split('/').at(-1);
     all[name] = filepath;
     return all;
   },
-  {index}
+  { index }
 );
 
 export default defineConfig({
   base: './',
-  resolve: {alias: {'~': resolve(__dirname, 'src')}},
+  resolve: { alias: { '~': resolve(__dirname, 'src') } },
   define: {
     __IS_VITE_BUILD__: 'true'
   },
   build: {
-    outDir: 'dist/examples',
+    outDir: 'dist',          
     emptyOutDir: true,
     rollupOptions: {
       input
